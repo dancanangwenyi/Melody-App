de7c7b323ca5c163d76ff3e226856b09e4894645
Dancan <dancangwe@gmail.com>
2018-09-06 11:52:42 +0200

    5th Commit: Established a TCP connection with my raspberry pi. The connection is working and now can send and receive a message.
    Signed-off-by: Dancan <dancangwe@gmail.com>

--- app/src/main/java/com/example/dancan/serverclienttester/PlaylistActivity.java
+++ app/src/main/java/com/example/dancan/serverclienttester/PlaylistActivity.java
@@ -2,6 +2,10 @@ package com.example.dancan.serverclienttester;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PlaylistActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class PlaylistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist);
+        ArrayList<String> items = new ArrayList<>();
+        items.add("tune 01");
+        items.add("tune 02");
+        items.add("tune 03");
+        items.add("tune 04");
+        items.add("tune 05");
+        items.add("tune 06");
+        items.add("tune 07");
+        items.add("tune 08");
+        items.add("tune 09");
+        items.add("tune 10");
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        ListView listView = findViewById(R.id.playlist);
+        listView.setAdapter(arrayAdapter);
     }
 }
