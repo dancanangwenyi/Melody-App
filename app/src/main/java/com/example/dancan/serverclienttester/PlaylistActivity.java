128eb29a98216887ec93831b4bf8073f22fa4b19
dancanmatara <dancangwe@gmail.com>
2018-09-23 14:54:10 +0200

    6th Commit: The real time activity modified to play only specific types of Notes. The program then sends a string containing the note Volume and duration.
    Signed-off-by: dancanmatara <dancangwe@gmail.com>

--- app/src/main/java/com/example/dancan/serverclienttester/PlaylistActivity.java
+++ app/src/main/java/com/example/dancan/serverclienttester/PlaylistActivity.java
@@ -1,18 +1,32 @@
 package com.example.dancan.serverclienttester;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 public class PlaylistActivity extends AppCompatActivity {
-
+    private TextView connectionInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist);
+
+        connectionInfo = findViewById(R.id.connInfo);
         ArrayList<String> items = new ArrayList<>();
         items.add("tune 01");
         items.add("tune 02");
@@ -24,9 +38,94 @@ public class PlaylistActivity extends AppCompatActivity {
         items.add("tune 08");
         items.add("tune 09");
         items.add("tune 10");
+        items.add("tune 11");
+        items.add("tune 12");
+        items.add("tune 13");
+        items.add("tune 14");
+        items.add("tune 15");
+        items.add("tune 16");
+        items.add("tune 17");
         ArrayAdapter<String> arrayAdapter =
                 new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         ListView listView = findViewById(R.id.playlist);
         listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Object entry = adapterView.getItemAtPosition(i);
+                sendMessage(entry.toString());
+            }
+        });
+    }
+
+    private void sendMessage(final String msg) {
+        final Handler handler = new Handler();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    Log.v("Tcp", "Clicked the button");
+                    Socket s = new Socket("196.24.183.81", 4448);
+                    Log.v("Tcp", "Got the Socket and host address");
+                    DataOutputStream oos = new DataOutputStream(s.getOutputStream());
+                    oos.writeUTF(msg);
+                    oos.flush();
+                    //oos.close();
+
+                    InputStreamReader reader = new InputStreamReader(s.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(reader);
+
+                    final String message = bufferedReader.readLine();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            ///String fromServer = connectionInfo.getText().toString();
+                            if (message.trim().length() != 0) {
+
+                                String reply = "\nReply from server: " + message;
+                                connectionInfo.setTextColor(Color.GREEN);
+                                connectionInfo.setText(reply);
+                            }
+                        }
+                    }); //end of handler runnable
+                    oos.close();
+                    s.close();
+
+                } catch (final UnknownHostException e) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            final String error = e.toString();
+                            connectionInfo.setTextColor(Color.RED);
+                            connectionInfo.setText(error);
+                        }
+                    });
+                    Log.v("Tcp", e.toString());
+                } catch (final IOException e) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            final String error = e.toString();
+                            connectionInfo.setText(error);
+                            connectionInfo.setTextColor(Color.RED);
+                        }
+                    });
+                    Log.v("Tcp", e.toString());
+                } catch (final Exception e) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            final String error = e.toString();
+                            connectionInfo.setTextColor(Color.RED);
+                            connectionInfo.setText(error);
+                        }
+                    });
+                    Log.v("Tcp", e.toString());
+                }
+            }
+        });
+
+        thread.start();
     }
 }
--- app/src/main/java/com/example/dancan/serverclienttester/RealtimeActivity.java
+++ app/src/main/java/com/example/dancan/serverclienttester/RealtimeActivity.java
@@ -1,5 +1,8 @@
 package com.example.dancan.serverclienttester;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -10,9 +13,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -29,31 +32,117 @@ public class RealtimeActivity extends AppCompatActivity implements View.OnClickL
         messageToBeSent = findViewById(R.id.clientMessage);
         Button button = findViewById(R.id.buttonSend);
         button.setOnClickListener(this);
+        Button buttonA = findViewById(R.id.buttonA);
+        buttonA.setOnClickListener(this);
+        Button buttonB = findViewById(R.id.buttonB);
+        buttonB.setOnClickListener(this);
+        Button buttonC = findViewById(R.id.buttonC);
+        buttonC.setOnClickListener(this);
+        Button buttonD = findViewById(R.id.buttonD);
+        buttonD.setOnClickListener(this);
+        Button buttonE = findViewById(R.id.buttonE);
+        buttonE.setOnClickListener(this);
+        Button buttonF = findViewById(R.id.buttonF);
+        buttonF.setOnClickListener(this);
+        Button buttonG = findViewById(R.id.buttonG);
+        buttonG.setOnClickListener(this);
+        Button buttonH = findViewById(R.id.buttonH);
+        buttonH.setOnClickListener(this);
+        Button buttonI = findViewById(R.id.buttonI);
+        buttonI.setOnClickListener(this);
+        Button buttonJ = findViewById(R.id.buttonJ);
+        buttonJ.setOnClickListener(this);
+        Button buttonK = findViewById(R.id.buttonK);
+        buttonK.setOnClickListener(this);
+        Button buttonL = findViewById(R.id.buttonL);
+        buttonL.setOnClickListener(this);
     }
-
     @Override
-    public void onClick(View view) {
+    public void onClick(View view) throws NullPointerException {
+        AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
         switch (view.getId()) {
             case R.id.buttonSend:
                 sendMessage(messageToBeSent.getText().toString());
                 messageToBeSent.setText("");
+                connectionError.setText("");
+                break;
+            case R.id.buttonA:
+                sendMessage("A" + currentVolume + "255");
+                messageToBeSent.setText("");
+                connectionError.setText("");
+                break;
+            case R.id.buttonB:
+                sendMessage("B" + currentVolume + "255");
+                messageToBeSent.setText("");
+                connectionError.setText("");
+                break;
+            case R.id.buttonC:
+                sendMessage("C" + currentVolume + "255");
+                messageToBeSent.setText("");
+                connectionError.setText("");
+                break;
+            case R.id.buttonD:
+                sendMessage("D" + currentVolume + "255");
+                messageToBeSent.setText("");
+                connectionError.setText("");
+                break;
+            case R.id.buttonE:
+                sendMessage("E" + currentVolume + "255");
+                messageToBeSent.setText("");
+                connectionError.setText("");
+                break;
+            case R.id.buttonF:
+                sendMessage("F" + currentVolume + "255");
+                messageToBeSent.setText("");
+                connectionError.setText("");
+                break;
+            case R.id.buttonG:
+                sendMessage("G" + currentVolume + "255");
+                messageToBeSent.setText("");
+                connectionError.setText("");
+                break;
+            case R.id.buttonH:
+                sendMessage("H" + currentVolume + "255");
+                messageToBeSent.setText("");
+                connectionError.setText("");
+                break;
+            case R.id.buttonI:
+                sendMessage("I" + currentVolume + "255");
+                messageToBeSent.setText("");
+                connectionError.setText("");
+                break;
+            case R.id.buttonJ:
+                sendMessage("J" + currentVolume + "255");
+                messageToBeSent.setText("");
+                connectionError.setText("");
+                break;
+            case R.id.buttonK:
+                sendMessage("K" + currentVolume + "255");
+                messageToBeSent.setText("");
+                connectionError.setText("");
+                break;
+            case R.id.buttonL:
+                sendMessage("L" + currentVolume + "255");
+                messageToBeSent.setText("");
+                connectionError.setText("");
                 break;
         }
-
     }
-
     private void sendMessage(final String msg) {
+        final String mes = msg.trim();
         final Handler handler = new Handler();
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
 
                 try {
-                    Log.v("Tcp", "Clicked the button");
-                    Socket s = new Socket("196.47.239.112", 4448);
+                    Log.v("Tcp", "Clicked the send button");
+                    Socket s = new Socket("196.42.78.98", 4448);
                     Log.v("Tcp", "Got the Socket and host address");
-                    ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
-                    oos.writeObject(msg);
+                    DataOutputStream oos = new DataOutputStream(s.getOutputStream());
+                    oos.writeUTF(mes);
+                    oos.flush();
 
                     InputStreamReader reader = new InputStreamReader(s.getInputStream());
                     BufferedReader bufferedReader = new BufferedReader(reader);
@@ -67,6 +156,7 @@ public class RealtimeActivity extends AppCompatActivity implements View.OnClickL
 
                                 String reply = fromServer + "\nReply from server: " + message;
                                 replyFromServer.setText(reply);
+                                replyFromServer.setTextColor(Color.GREEN);
                             }
                         }
                     }); //end of handler runnable
@@ -79,6 +169,7 @@ public class RealtimeActivity extends AppCompatActivity implements View.OnClickL
                         @Override
                         public void run() {
                             final String error = e.toString();
+                            connectionError.setTextColor(Color.RED);
                             connectionError.setText(error);
                         }
                     });
@@ -88,6 +179,7 @@ public class RealtimeActivity extends AppCompatActivity implements View.OnClickL
                         @Override
                         public void run() {
                             final String error = e.toString();
+                            connectionError.setTextColor(Color.RED);
                             connectionError.setText(error);
                         }
                     });
@@ -97,6 +189,7 @@ public class RealtimeActivity extends AppCompatActivity implements View.OnClickL
                         @Override
                         public void run() {
                             final String error = e.toString();
+                            connectionError.setTextColor(Color.RED);
                             connectionError.setText(error);
                         }
                     });
